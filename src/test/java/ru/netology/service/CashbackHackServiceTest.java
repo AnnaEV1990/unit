2545f1ed5fac44782dbff7893ae599fedd0bb50a
Anna Evseykina <anytta9032@yandex.ru>
2024-04-02 16:24:41 +0700

    add Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,32 +1,35 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void testWhenNothingWasSpent() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void testWhenCalculatedCorrectly() {
-        org.testng.Assert.assertEquals(service.remain(400), 600);
+        org.junit.Assert.assertEquals(service.remain(400), 600);
     }
 
-    @org.testng.annotations.Test
-    public void testAmountmoreBoundary() {
-        org.testng.Assert.assertEquals(service.remain(1200), 800);
+    @Test
+    public void testAmountMoreBoundary() {
+        org.junit.Assert.assertEquals(service.remain(1200), 800);
     }
 
-    @org.testng.annotations.Test
+    @Test
+
     public void testWhenAmountEqualsBoundary() {
         org.testng.Assert.assertEquals(service.remain(1000), 0);
     }
 
-    @org.testng.annotations.Test
-    public void testAmoutNegative() {
+
+    @Test
+    public void testAmountNegative() {
         org.testng.Assert.assertEquals(service.remain(-1), 0);
     }
 }
