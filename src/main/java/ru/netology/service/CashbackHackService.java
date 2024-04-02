d9f893054ec630d938535de0a89d7a8678816dc2
Anna Evseykina <anytta9032@yandex.ru>
2024-04-02 12:39:40 +0700

    add CashbackHackServiceTest

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -6,4 +6,5 @@ public class CashbackHackService {
         return boundary - amount % boundary;
     }
 
+
 }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,11 +5,13 @@ import org.testng.annotations.Test;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void testRemainBoundaryValuesOne() {
-        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
-            service.remain(-1);
-        });
+ //   @Test
+  //  public void testRemainBoundaryValuesTwo() {
+  //      org.testng.Assert.assertEquals(service.remain(0), 1000);
+   // }
 
+    @org.testng.annotations.Test
+    public void testRemainCriticalPath() {
+        org.testng.Assert.assertEquals(service.remain(400), 600);
     }
 }
