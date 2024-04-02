25a04b9dd7019951836f3008d1cd93621e75ba1d
Anna Evseykina <anytta9032@yandex.ru>
2024-04-02 14:30:32 +0700

    change CashbackHackServiceTest and CashbackHackService

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,13 +5,28 @@ import org.testng.annotations.Test;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
- //   @Test
-  //  public void testRemainBoundaryValuesTwo() {
-  //      org.testng.Assert.assertEquals(service.remain(0), 1000);
-   // }
+    @Test
+    public void testWhenNothingWasSpent() {
+        org.testng.Assert.assertEquals(service.remain(0), 1000);
+    }
 
     @org.testng.annotations.Test
-    public void testRemainCriticalPath() {
+    public void testWhenCalculatedCorrectly() {
         org.testng.Assert.assertEquals(service.remain(400), 600);
+    }
+
+    @org.testng.annotations.Test
+    public void testAmountmoreBoundary() {
+        org.testng.Assert.assertEquals(service.remain(1200), 800);
+    }
+
+    @org.testng.annotations.Test
+    public void testWhenAmountEqualsBoundary() {
+        org.testng.Assert.assertEquals(service.remain(1000), 0);
+    }
+
+    @org.testng.annotations.Test
+    public void testAmoutNegative() {
+        org.testng.Assert.assertEquals(service.remain(-1), 0);
     }
 }
